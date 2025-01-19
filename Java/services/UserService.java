3323c17b7664d6d713f378bd5e1ee5db389784c3
asreena04 <asreena04@gmail.com>
2025-01-19 08:44:23 +0800

    add java class

---
+++ Java/services/UserService.java
@@ -0,0 +1,23 @@
+package services;
+
+import elements.UserBean;
+
+public interface UserService {
+
+
+	public String registerUser(String userName, Long mobileNo, String emailId, String address, int pinCode,
+			String password);
+
+	public String registerUser(UserBean user);
+
+	public boolean isRegistered(String emailId);
+
+	public String isValidCredential(String emailId, String password);
+
+	public UserBean getUserDetails(String emailId, String password);
+
+	public String getFName(String emailId);
+
+	public String getUserAddr(String userId);
+
+}
